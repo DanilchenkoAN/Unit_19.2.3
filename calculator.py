150e8ae270eb71ea23cf7fe0ebab94cdaf332200
ANDREY DANILCHENKO <100102296+DanilchenkoAN@users.noreply.github.com>
2022-05-17 22:42:51 +1000

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+class Calculator:
+    def mult (self, x, y):
+        return x * y
+
+    def div (self, x, y):
+        return x / y
+
+    def sum (self, x, y):
+        return x + y
+
+    def sub (self, x, y):
+        return x - y
